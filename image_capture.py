6845584b3bba096106a03777bed7f3c43bd32c7c
TheIllusionist77 <113849310+TheIllusionist77@users.noreply.github.com>
2024-09-29 16:56:06 -0400

    Update image_capture.py

--- image_capture.py
+++ image_capture.py
@@ -2,7 +2,7 @@ import cv2
 from picamera2 import Picamera2
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration(main={"format": "XRGB8888", "size": (640, 480)}))
+picam2.configure(picam2.create_preview_configuration(main = {"format": "XRGB8888", "size": (640, 480)}))
 picam2.start() 
 
 count = 0
